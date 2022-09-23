1b68ef4236638141d1ac36f50e5f06f93648ae94
Migovich <migovichdev@gmail.com>
2022-09-23 17:30:19 +0300

    Initial commit

---
+++ MineDetect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MineDetect
+//
+//  Created by Migovich on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
