a08838f02abdfc46a6e82419443cfe30a769ef88
Vitalii <vlprimary@gmail.com>
2022-09-24 13:57:06 +0300

    integrated map fetching service

--- MineDetect/Services/MinesFetcher.swift
+++ MineDetect/Services/MinesFetcher.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 protocol MinesFetcherDelegate: AnyObject {
     func didFetchMines(_ data: [MineModel])
 }
@@ -49,10 +50,9 @@ class MinesFetcher: NSObject {
     }
     
     private let mockMines: [MineModel] = [
-        MineModel(name: "Mine", location: Location(latitude: 50.4538916, longitude: 30.4773090)),
-        MineModel(name: "Bomb", location: Location(latitude: 50.4539439, longitude: 30.4758224)),
-        MineModel(name: "Unknown", location: Location(latitude: 50.4559810, longitude: 30.4794799)),
-        MineModel(name: "Granade", location: Location(latitude: 50.4507702, longitude: 30.4825436))
+        MineModel(name: "Протипіхотна осколкова міна ПОМЗ", description: "За словами Тетяни Вельшиної, одна з найнебезпечніших осколкових мін — ОЗМ-72: За розміром вона, як літрова банка. Важить 5 кілограмів. Усередині має 2400 осколків, які розлітаються на 50 метрів. Людина зачіпає розтяжку, міна вистрибує на висоту 1 метр через ”вишибний” заряд і тоді розривається корпус. Шансів вижити майже немає. \nПротипіхотні міни заборонені Оттавською конвенцією із 1999 року.", imageUrl: "https://cdn4.suspilne.media/images/resize/1040x1.78/8c1ee21fdb2f0eb7.jpg", location: Location(latitude: 50.4538916, longitude: 30.4773090)),
+        MineModel(name: "Протитранспортна міна ТМ-62", description: "Часто під протитранспортні міни можуть встановлювати протипіхотні. У разі вибуху — шансів вижити практично немає.", imageUrl: "https://cdn4.suspilne.media/images/resize/1040x1.78/d3e717eb3b36a8e7.jpg", location: Location(latitude: 50.4539439, longitude: 30.4758224)),
+        MineModel(name: "Міна “Метелик”", description: "Особливу небезпеку становить міна ”Метелик” (також її називають ”Пелюстка”, — ред.). Ці міни розміром у 12 сантиметрів і вагою близько 80 грамів кожна розкидають із касетних боєприпасів. Вони розриваються або при контакті, або мають часовий детонатор, тобто елемент самоліквідації. Через кілька годин міна може самостійно вибухнути, — каже Тетяна Вельшина.", imageUrl: "https://cdn4.suspilne.media/images/resize/1040x1.78/05604f6932736e1b.jpg", location: Location(latitude: 50.4559810, longitude: 30.4794799))
     ]
     
     func fetchMines(_ completion: (() -> Void)? = nil) {
