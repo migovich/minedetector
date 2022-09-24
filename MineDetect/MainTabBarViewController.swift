a86e06af134fcc15012169ec9d9f1ab4fcb86399
Migovich <migovichdev@gmail.com>
2022-09-24 19:06:00 +0300

    Minor changes

--- MineDetect/MainTabBarViewController.swift
+++ MineDetect/MainTabBarViewController.swift
@@ -25,24 +25,18 @@ class MainTabBarController: UITabBarController {
     // MARK: Functions
     private func setupViewControllers() {
         let mapVC = UIStoryboard(name: MapViewController.className, bundle: nil).instantiateViewController(identifier: MapViewController.className)
-        let photoVC = UIStoryboard(name: PhotoViewController.className, bundle: nil).instantiateViewController(identifier: PhotoViewController.className)
         let settingsVC = UIStoryboard(name: SettingsViewController.className, bundle: nil).instantiateViewController(identifier: SettingsViewController.className)
         
-        let mapItem = UITabBarItem(title: "Map",
+        let mapItem = UITabBarItem(title: "Мапа",
                                    image: UIImage(),
                                    selectedImage: UIImage())
         mapItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
-        let photoItem = UITabBarItem(title: "Camera",
-                                     image: UIImage(),
-                                     selectedImage: UIImage())
-        photoItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
-        let settingsItem = UITabBarItem(title: "Settings",
+        let settingsItem = UITabBarItem(title: "Налаштування",
                                      image: UIImage(),
                                      selectedImage: UIImage())
         settingsItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -15)
         
         mapVC.tabBarItem = mapItem
-        photoVC.tabBarItem = photoItem
         settingsVC.tabBarItem = settingsItem
         
         let controllers = [mapVC, settingsVC]
